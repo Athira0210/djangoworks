213ab46d8370b2cf16d355eb5561477404f78839
Athira0210 <106605121+Athira0210@users.noreply.github.com>
2022-08-02 10:54:07 +0530

    productapiurl

---
+++ urls.py
@@ -0,0 +1,32 @@
+"""ecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from products import views
+from productapi.views import Productsview,ProductDetailView,ProductModelView,ProductdetailModelview,ProductViewsetview
+from rest_framework.routers import DefaultRouter
+router=DefaultRouter()
+router.register('api/v3/products',ProductViewsetview,basename='products')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('product/list/',views.ProductView.as_view()),
+    path('myg/products/',Productsview.as_view()),
+    path('myg/products/<int:id>',ProductDetailView.as_view()),
+    path('api/v2/products/',ProductModelView.as_view()),
+    path('api/v2/products/<int:id>',ProductdetailModelview.as_view())
+]+router.urls
