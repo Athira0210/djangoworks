022fc6fe962d8d892d2dcd7658cd7afdb6a7988a
Athira0210 <106605121+Athira0210@users.noreply.github.com>
2022-08-09 11:10:37 +0530

    productapiurl

--- urls.py
+++ urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products import views
-from productapi.views import Productsview,ProductDetailView,ProductModelView,ProductdetailModelview,ProductViewsetview
+from productapi.views import Productsview,ProductDetailView,ProductModelView,ProductdetailModelview,ProductViewsetview,ProductModelViewsetView,UserModelViewsetView
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 router=DefaultRouter()
 router.register('api/v3/products',ProductViewsetview,basename='products')
+router.register('api/v4/products',ProductModelViewsetView,basename='mproducts')
+router.register('account/signup',UserModelViewsetView,basename='users')
 
 
 urlpatterns = [
@@ -28,5 +31,6 @@ urlpatterns = [
     path('myg/products/',Productsview.as_view()),
     path('myg/products/<int:id>',ProductDetailView.as_view()),
     path('api/v2/products/',ProductModelView.as_view()),
-    path('api/v2/products/<int:id>',ProductdetailModelview.as_view())
+    path('api/v2/products/<int:id>',ProductdetailModelview.as_view()),
+    path('api/v4/token',obtain_auth_token)
 ]+router.urls
